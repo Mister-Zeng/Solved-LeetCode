c4fce38d1f3aff957f94d1fa9a870fec1a44210d
Jason Zeng <officialjasonzeng@gmail.com>
2022-05-20 11:36:17 -0400

    Update 1-two-sum.js

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,8 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         for(let j = i + 1; j < nums.length; j++){
