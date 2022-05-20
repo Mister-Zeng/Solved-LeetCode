6e1f87fe8bc7fbb687fba6ff77844b391a00bd25
Jason Zeng <officialjasonzeng@gmail.com>
2022-05-20 12:02:27 -0400

    Update 1-two-sum.js

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,9 +1,3 @@
-//Return the number (count) of vowels in the given string.
-
-//We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
-//The input string will only consist of lower case letters and/or spaces.
-
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         for(let j = i + 1; j < nums.length; j++){
