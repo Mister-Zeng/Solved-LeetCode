f46b1de598d4ee886f817cd6b64a953b44471c21
Jason Zeng <officialjasonzeng@gmail.com>
2022-08-23 16:03:02 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (81.39%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -21,4 +21,23 @@ class Solution {
         }
         return prev;
     }
+    
+    /*
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+    
+    public ListNode reverse(ListNode head, ListNode prev){
+        if(head == null) {
+            return prev;
+        }
+        
+        ListNode temp = head.next;
+        head.next = prev;
+        prev = head;
+        head = temp;
+        
+        return reverse(head, prev)
+    }
+    */
 }
