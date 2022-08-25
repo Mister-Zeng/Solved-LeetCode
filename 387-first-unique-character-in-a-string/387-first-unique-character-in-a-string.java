f2fbbd82295842f60322921d70aac36edc368ce9
Jason Zeng <officialjasonzeng@gmail.com>
2022-08-25 15:25:13 -0400

    Time: 9 ms (88.03%), Space: 48.8 MB (62.21%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,9 +1,28 @@
 class Solution {
     public int firstUniqChar(String s) {
+        int[] list = new int[26];
+        
+        for(int i = 0 ; i < s.length() ; i++) {
+            list[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0 ; i < s.length() ; i++) {
+            if(list[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+/*
+class Solution {
+    public int firstUniqChar(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         
         for(int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
+ 
             if(!map.containsKey(curr)) {
                 map.put(curr, i);
             } else {
@@ -12,8 +31,8 @@ class Solution {
         }
         
         int min = Integer.MAX_VALUE;
-        for(char c: map.keySet()){
-            if(map.get(c) > -1 && map.get(c) < min){
+        for(char c : map.keySet()) {
+            if(map.get(c) > -1 && map.get(c) < min) {
                 min = map.get(c);
             }
         }
@@ -21,3 +40,4 @@ class Solution {
         return min == Integer.MAX_VALUE ? -1 : min;
     }
 }
+*/
