5cf8e906a452aa0416815cf9a9308d26033c0e67
Jason Zeng <officialjasonzeng@gmail.com>
2022-08-25 16:08:10 -0400

    Time: 1 ms (10.66%), Space: 40.4 MB (80.75%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,5 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
+
+/*
+class Solution {
+    public int climbStairs(int n) {
         int a = 1;
         int b = 1;
         int c;
@@ -12,3 +27,4 @@ class Solution {
         return b;
     }
 }
+*/
