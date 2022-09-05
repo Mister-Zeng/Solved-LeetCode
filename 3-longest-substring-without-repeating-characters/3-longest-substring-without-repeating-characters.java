c4f965dffcc3242befebb67792ed2939d2bb5371
Jason Zeng <officialjasonzeng@gmail.com>
2022-09-05 15:33:41 -0400

    Time: 7 ms (86.20%), Space: 42.6 MB (89.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -6,12 +6,15 @@ class Solution {
         int left = 0, right = 0;
         while(right < s.length()) {
             while(window.contains(s.charAt(right))) {
-                window.remove(s.charAt(left++));
+                window.remove(s.charAt(left));
+                left++;
             }
-            window.add(s.charAt(right++));
+            window.add(s.charAt(right));
+            right++;
             maxLength = Math.max(maxLength, right - left);
         }
         
         return maxLength;
     }
 }
+
