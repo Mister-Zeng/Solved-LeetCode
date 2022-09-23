93f5a55dc879b25fe8343ad2776e04698fd8eba0
Jason Zeng <officialjasonzeng@gmail.com>
2022-09-23 14:42:19 -0400

    Time: 2 ms (88.78%), Space: 46.4 MB (92.27%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] countBits(int n) {
+        // an array to store each bit 
+        int[] ans = new int[n + 1]; // n + 1 since we are including the index of 0
+        
+        // loop through from 0 to n 
+        for(int i = 0; i <= n ; i++) {
+            ans[i] = counts(i);
+        }
+        
+        return ans;
+    }
+    
+    // find amount of 1s in the bits
+    public int counts(int num) {
+        // variable to store counts 
+        int count = 0;
+        
+        // using & bitwise operator 
+        while(num != 0) {
+            num &= num - 1;
+            count++;
+        }
+        return count;
+    }
+}
