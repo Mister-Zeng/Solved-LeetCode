bfccd1e5ba5a22802560b8eef9481c8b8cf15a9e
Jason Zeng <officialjasonzeng@gmail.com>
2022-09-29 11:25:38 -0400

    Time: 22 ms (98.53%), Space: 46.7 MB (89.10%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> result = new HashSet<>();
+        LinkedList<List<Integer>> result = new LinkedList<>();
 
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
                 int left = i + 1;
                 int right = nums.length - 1;
                 int target = 0 - nums[i];
@@ -16,6 +17,8 @@ class Solution {
                         sol.add(nums[left]);
                         sol.add(nums[right]);
                         result.add(sol);
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
                         left++;
                         right--;
                     } else if (nums[left] + nums[right] > target) {
@@ -25,6 +28,7 @@ class Solution {
                     }
                 }
             }
-        return new ArrayList(result);
+        }
+        return result;
     }
 }
