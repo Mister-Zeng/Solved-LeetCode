fc676b62e3a9f9e0cf308af1c0c181416f4d2826
Jason Zeng <officialjasonzeng@gmail.com>
2022-09-29 11:15:50 -0400

    Time: 37 ms (51.29%), Space: 59.8 MB (33.06%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+
         Arrays.sort(nums);
-        LinkedList<List<Integer>> list = new LinkedList<>();
-        
-        for(int i = 0; i < nums.length - 2; i++) {
-            if(i == 0 ||( i > 0 && nums[i] != nums[i - 1])) {
-                int target = 0 - nums[i];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
                 int left = i + 1;
                 int right = nums.length - 1;
-                
-                while(left < right) {
-                    if(nums[left] + nums[right] == target) {
-                        ArrayList<Integer> addList = new ArrayList<>();
-                        addList.add(nums[i]);
-                        addList.add(nums[left]);
-                        addList.add(nums[right]);
-                        list.add(addList);
-                        while(left < right && nums[left] == nums[left + 1]) {
+                int target = 0 - nums[i];
+
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        ArrayList<Integer> sol = new ArrayList<>();
+                        sol.add(nums[i]);
+                        sol.add(nums[left]);
+                        sol.add(nums[right]);
+                        result.add(sol);
+                        while (left < right && nums[left] == nums[left + 1])
                             left++;
-                        }
-                        while(left < right && nums[right] == nums[right - 1]) {
+                        while (left < right && nums[right] == nums[right - 1])
                             right--;
-                        }
                         left++;
                         right--;
                     } else if (nums[left] + nums[right] > target) {
@@ -32,7 +31,6 @@ class Solution {
                 }
             }
         }
-        
-        return list;
+        return result;
     }
 }
