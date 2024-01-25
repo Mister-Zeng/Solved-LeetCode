16d23f743e2b179de559654eb78f611226748ba4
Jason Zeng <officialjasonzeng@gmail.com>
2024-01-25 15:42:22 -0500

    Time: 0 ms (100.00%), Space: 40 MB (77.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int a = 1; 
+        int b = 1;
+        int c = 0;
         
-        for(int i = 2; i <= n ; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for(int i = 0; i < n - 1; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
         
-        return dp[n];
+        return b;
     }
 }
