d199d0eb987042f2bcb5c88731c513f81fe45185
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-03 12:44:41 -0500

    Time: 6 ms (98.40%), Space: 42.7 MB (17.84%) - LeetHub

--- 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.java
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.java
@@ -9,8 +9,8 @@ class FizzBuzz {
 
     // printFizz.run() outputs "fizz".
     public void fizz(Runnable printFizz) throws InterruptedException {
-        while(i <= n ) { 
-            synchronized(this) { 
+        synchronized(this) { 
+            while(i <= n ) {
                 if(i % 3 == 0 && i % 5 != 0) {
                     printFizz.run();
                     i++;
@@ -24,8 +24,8 @@ class FizzBuzz {
 
     // printBuzz.run() outputs "buzz".
     public void buzz(Runnable printBuzz) throws InterruptedException {
-        while(i <= n ) { 
-            synchronized(this) { 
+        synchronized(this) { 
+            while(i <= n ) {
                 if(i % 3 != 0 && i % 5 == 0) {
                     printBuzz.run();
                     i++;
@@ -39,8 +39,8 @@ class FizzBuzz {
 
     // printFizzBuzz.run() outputs "fizzbuzz".
     public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-        while(i <= n ) { 
-            synchronized(this) { 
+        synchronized(this) { 
+            while(i <= n ) {
                 if(i % 15 == 0) {
                     printFizzBuzz.run();
                     i++;
@@ -54,8 +54,8 @@ class FizzBuzz {
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void number(IntConsumer printNumber) throws InterruptedException {
-        while(i <= n ) { 
-            synchronized(this) { 
+        synchronized(this) { 
+            while(i <= n ) {
                 if(i % 3 != 0 && i % 5 != 0) {
                     printNumber.accept(i);
                     i++;
