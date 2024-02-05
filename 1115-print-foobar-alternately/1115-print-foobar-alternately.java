0460f23ac2515ab4bee5cbeb964b8fe9a079a365
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-05 16:07:48 -0500

    Time: 21 ms (98.67%), Space: 43.2 MB (78.98%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -0,0 +1,40 @@
+class FooBar {
+    private int n;
+    private boolean run;
+    
+    public FooBar(int n) {
+        this.n = n;
+        this.run = true;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        synchronized(this) {
+            for (int i = 0; i < n; i++) {
+                if(!run) {
+                    wait();
+                } 
+                
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                run = !run; 
+                notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        synchronized(this) {
+            for (int i = 0; i < n; i++) {
+                if(run) {
+                    wait();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+        	    printBar.run();
+                run = !run;
+                notifyAll();
+            }
+        }
+    }
+}
