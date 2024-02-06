031f2a97008c9265f01490c3769385363017eb94
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-06 13:31:28 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (91.41%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,35 +1,39 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> sStack = new Stack<>();
-        Stack<Character> tStack = new Stack<>();
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder t1 = new StringBuilder();
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0;
+        int skipT = 0; 
         
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '#' && sStack.size() > 0) {
-                sStack.pop();
-            } else if(s.charAt(i) != '#') {
-                sStack.push(s.charAt(i));
+        while(i >= 0 || j >= 0) {
+            while(i >= 0) {
+                if(s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if(skipS > 0) {
+                    skipS--;
+                    i--;
+                } else break;
             }
-        }
-        
-        for(int j = 0; j < t.length(); j++) {
-            if(t.charAt(j) == '#' && tStack.size() > 0) {
-                tStack.pop();
-            } else if(t.charAt(j) != '#') {
-                tStack.push(t.charAt(j));
+            
+            while(j >= 0) {
+                if(t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if(skipT > 0) {
+                    skipT--;
+                    j--;
+                } else break;
             }
+            
+            if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) return false;
+            
+            if((i >= 0 ) != (j >= 0)) return false;
+            
+            i--;
+            j--;
         }
         
-        while(!sStack.isEmpty()){
-            s1.append(sStack.pop());
-        }
-        
-        while(!tStack.isEmpty()){
-            t1.append(tStack.pop());
-        }
-        
-        return s1.toString().equals(t1.toString());
-        
+        return true;
     }
 }
