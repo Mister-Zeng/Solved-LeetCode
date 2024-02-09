69b150365e8c9cbec13a88c3463d0c1cf66cda40
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-09 17:42:18 -0500

    Time: 15 ms (81.70%), Space: 44.4 MB (47.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,17 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0; 
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
         
-        LinkedList<Integer> queue = new LinkedList<>();
-        // tracking sum in queue
-        queue.add(0);
-        int coinCount = 0; 
-        boolean[] visited = new boolean[amount + 1];
-        
-        while(!queue.isEmpty()) {
-            int length = queue.size();
-            
-            for(int i = 0; i < length; i++) {
-                int sum = queue.poll();
-                
-                if(sum == amount) return coinCount;
-                
-                if(sum > amount || visited[sum]) continue;
-                
-                visited[sum] = true; 
-                
-                for(int c : coins) {
-                    queue.add(sum + c);
+        for(int i = 1; i <= amount; i++) {
+            for(int c : coins) {
+                if(i >= c) {
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
                 }
             }
-            
-            coinCount++;
-        } 
+        }
         
-        return -1; 
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
