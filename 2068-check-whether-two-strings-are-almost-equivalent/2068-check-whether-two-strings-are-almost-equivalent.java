416a2959675b381ec972e87f485561b226ad1e83
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-10 18:31:19 -0500

    Time: 2 ms (37.19%), Space: 42 MB (12.81%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -1,31 +1,18 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-        Map<Character, Integer> mapOne = new HashMap<>();
-        Map<Character, Integer> mapTwo = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         
         for(char c : word1.toCharArray()) {
-            mapOne.put(c, mapOne.getOrDefault(c, 0) + 1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
         for(char c : word2.toCharArray()) {
-            mapTwo.put(c, mapTwo.getOrDefault(c, 0) + 1);
+            map.put(c, map.getOrDefault(c, 0) - 1);
         }
         
-        for(char c : mapOne.keySet()) {
-            int mapTwoCount = mapTwo.getOrDefault(c, 0);
-            if(Math.abs(mapOne.get(c) - mapTwoCount) > 3) {
-                return false;
-            }
+        for(int i : map.values()) {
+            if(i > 3 || i < -3) return false;
         }
-        
-        for(char c : mapTwo.keySet()) {
-            int mapOneCount = mapOne.getOrDefault(c, 0);
-            if(Math.abs(mapTwo.get(c) - mapOneCount) > 3) {
-                return false;
-            }
-        }
-        
-        
         
         return true;
     }
