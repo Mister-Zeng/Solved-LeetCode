823b9c1ad76539779e17664b58422e72a895da28
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-12 18:39:37 -0500

    Time: 1 ms (100.00%), Space: 41.7 MB (53.61%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -3,13 +3,11 @@ class Solution {
         int[] charCount = new int[128];
         int result = 0;
         
-        for (char c: s.toCharArray())
-            charCount[c]++;
+        for (char c: s.toCharArray()) charCount[c]++;
         
         for(int c : charCount) {
             result += c / 2 * 2;
-            if (result % 2 == 0 && c % 2 == 1)
-                result++;
+            if (result % 2 == 0 && c % 2 == 1) result++;
         }
         
         return result;
