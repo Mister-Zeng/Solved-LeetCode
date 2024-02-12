d372605afd1cc2233901a0b3332ab5b3f4843f5e
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-12 17:57:33 -0500

    Time: 26 ms (45.41%), Space: 46.5 MB (90.69%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,23 +1,27 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int degree = 0; 
-        
-        Map<Integer, Integer> firstSeenMap = new HashMap<>();
-        int minCount = 0;
-        
-        for(int i = 0; i < nums.length; i++) {
-            int current = nums[i];
-            firstSeenMap.putIfAbsent(current, i);
-            countMap.put(current, countMap.getOrDefault(current, 0) + 1);
-            if(countMap.get(current) > degree) {
-                degree = countMap.get(current);
-                minCount = i - firstSeenMap.get(current) + 1;
-            } else if(countMap.get(current) == degree) {
-                minCount = Math.min(minCount, i - firstSeenMap.get(current) + 1);
+        Map<Integer, Integer> cnt = new HashMap<>();
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        int degree = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            int v = nums[i];
+            cnt.put(v, cnt.getOrDefault(v, 0) + 1);
+            degree = Math.max(degree, cnt.get(v));
+            if (!left.containsKey(v)) {
+                left.put(v, i);
+            }
+            right.put(v, i);
+        }
+        int ans = 1000000;
+        for (int v : nums) {
+            if (cnt.get(v) == degree) {
+                int t = right.get(v) - left.get(v) + 1;
+                if (ans > t) {
+                    ans = t;
+                }
             }
         }
-        
-        return minCount;
+        return ans;
     }
 }
