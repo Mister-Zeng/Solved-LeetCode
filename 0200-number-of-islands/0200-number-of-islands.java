125216907c721d879f3f5e0396f0fea81011ce55
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-12 14:43:58 -0500

    Time: 3 ms (88.39%), Space: 49.2 MB (70.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -11,7 +11,7 @@ class Solution {
         for(int r = 0; r < rLength; r++) {
             for(int c = 0; c < cLength; c++) {
                 if(grid[r][c] == '1') {
-                    bfs(grid, r, c);
+                    dfs(grid, r, c);
                     islandCount++;
                 }
             }
@@ -20,35 +20,46 @@ class Solution {
         return islandCount;
     }
     
-    public void bfs(char[][] grid, int r, int c) {
-        grid[r][c] = '0';
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { r , c });
+    public void dfs(char[][] grid, int r, int c) {
+        if(r < 0 || c < 0 || r >= rLength || c >= cLength 
+           || grid[r][c] == '0') return;
         
-        while(!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int row = current[0];
-            int column = current[1]; 
-            
-            if(row - 1 >= 0 && grid[row - 1][column] == '1') {
-                queue.add(new int[] { row - 1, column });
-                grid[row - 1][column] = '0';
-            }
-            
-            if(row + 1 < grid.length && grid[row + 1][column] == '1') {
-                queue.add(new int[] { row + 1, column});
-                grid[row + 1][column] = '0';
-            }
-            
-            if(column - 1 >= 0 && grid[row][column - 1] == '1') {
-                queue.add(new int[] { row, column - 1 });
-                grid[row][column - 1] = '0';
-            }
-            
-            if(column + 1 < grid[row].length && grid[row][column + 1] == '1') {
-                queue.add(new int[]{ row, column + 1});
-                grid[row][column + 1] = '0';
-            }
-        }
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
     }
+    
+//     public void bfs(char[][] grid, int r, int c) {
+//         grid[r][c] = '0';
+//         Queue<int[]> queue = new LinkedList<>();
+//         queue.add(new int[] { r , c });
+        
+//         while(!queue.isEmpty()) {
+//             int[] current = queue.poll();
+//             int row = current[0];
+//             int column = current[1]; 
+            
+//             if(row - 1 >= 0 && grid[row - 1][column] == '1') {
+//                 queue.add(new int[] { row - 1, column });
+//                 grid[row - 1][column] = '0';
+//             }
+            
+//             if(row + 1 < grid.length && grid[row + 1][column] == '1') {
+//                 queue.add(new int[] { row + 1, column});
+//                 grid[row + 1][column] = '0';
+//             }
+            
+//             if(column - 1 >= 0 && grid[row][column - 1] == '1') {
+//                 queue.add(new int[] { row, column - 1 });
+//                 grid[row][column - 1] = '0';
+//             }
+            
+//             if(column + 1 < grid[row].length && grid[row][column + 1] == '1') {
+//                 queue.add(new int[]{ row, column + 1});
+//                 grid[row][column + 1] = '0';
+//             }
+//         }
+//     }
 }
