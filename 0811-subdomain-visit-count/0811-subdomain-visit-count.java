c3d638ee0762739a7aedbf1d785db6484112f809
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-13 16:02:29 -0500

    Time: 9 ms (99.14%), Space: 45.3 MB (63.76%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.java
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<String> subdomainVisits(String[] cpdomains) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> counter = new HashMap<>();
         List<String> result = new ArrayList<>();
         
         for(String s : cpdomains) {
             String[] split = s.split(" ");
-            map.put(split[1], map.getOrDefault(split[1], 0) + Integer.parseInt(split[0]));
-            int decimalIndex = split[1].indexOf('.');
+            
+            counter.put(split[1], counter.getOrDefault(split[1], 0) + Integer.parseInt(split[0]));
+            int decimalIndex = split[1].indexOf(".");
+            
             while(decimalIndex != -1) {
                 split[1] = split[1].substring(decimalIndex + 1);
-                map.put(split[1], map.getOrDefault(split[1], 0) + Integer.parseInt(split[0]));
-                decimalIndex = split[1].indexOf('.');
+                counter.put(split[1], counter.getOrDefault(split[1], 0) + Integer.parseInt(split[0]));
+                decimalIndex = split[1].indexOf(".");
             }
-            
         }
         
-        for(String s : map.keySet()) {
+        for(Map.Entry<String, Integer> entry : counter.entrySet()) {
             StringBuilder sb = new StringBuilder();
-            sb.append(map.get(s)).append(" ").append(s);
+            sb.append(entry.getValue()).append(" ").append(entry.getKey());
             result.add(sb.toString());
         }
         
