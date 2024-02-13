7558e4d7fbf8c00526ab78e653e20eceaa6463ee
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-13 10:52:39 -0500

    Time: 0 ms (100.00%), Space: 41.4 MB (91.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,8 +1,8 @@
 class Solution {
+
     public void sortColors(int[] nums) {
         int left = 0, mid = 0, right = nums.length - 1;
         while (mid <= right) {
-            System.out.println(nums[mid]);
             if (nums[mid] == 1) mid++; else if (nums[mid] == 0) {
                 swap(nums, mid++, left++);
             } else if (nums[mid] == 2) {
