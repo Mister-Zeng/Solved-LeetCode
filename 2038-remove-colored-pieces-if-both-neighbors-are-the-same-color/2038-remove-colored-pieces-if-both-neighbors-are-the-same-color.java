0b3fa7d165cd88bda5593cd2113d310f57c85c58
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-13 15:44:26 -0500

    Time: 18 ms (23.77%), Space: 44.8 MB (85.29%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 1; i < colors.length() - 1; i++) {
             if(colors.charAt(i) == colors.charAt(i - 1) && colors.charAt(i) == colors.charAt(i + 1)) {
                 if(colors.charAt(i) == 'A') countA++;
-                else countB++;
+                else if(colors.charAt(i) == 'B') countB++;
             } 
         }
         
