adc1af65bfbb89393aacaf91a337d66bdfe596b7
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-15 15:49:57 -0500

    Time: 24 ms (97.77%), Space: 51.6 MB (50.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -14,18 +14,18 @@ class Solution {
             while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum < 0) {
-                    ++left;
+                    left++;
                 } else if(sum > 0) {
-                    --right;
+                    right--;
                 } else {
                     result.add(List.of(nums[i], nums[left++], nums[right--]));
                     
                     while(left < right && nums[left] == nums[left - 1]) {
-                        ++left;
+                        left++;
                     }
                     
                     while(left < right && nums[right] == nums[right + 1]) {
-                        --right;
+                        right--;
                     }
                 }
             }
