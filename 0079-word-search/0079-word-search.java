8fcaa21804d77741be8d866fdcfb5a5e6271134f
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-23 14:46:15 -0500

    Time: 33 ms (96.64%), Space: 44.8 MB (22.89%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,7 +5,9 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         rLength = board.length;
         cLength = board[0].length;
-
+        
+        if(!checkWordCharactersInBoard(board, rLength, cLength, word) ) return false;   
+        
         for(int i = 0; i < rLength; i++) {
             for(int j = 0; j < cLength; j++) {
                 if(search(board, new boolean[rLength][cLength], word, i, j, 0)) {
@@ -38,4 +40,18 @@ class Solution {
         
         return false;
     }
+    
+    public boolean checkWordCharactersInBoard(char[][] board, int rows, int cols, String word){             
+		List<Character> wordList = new LinkedList<Character>();
+		for(char ch : word.toCharArray()){ wordList.add(ch); }
+		for(int r = 0; r < rows; r++){
+			for(int c = 0; c < cols; c++){
+				char ch = board[r][c];
+				if(wordList.size() == 0){ return true; }
+				if(wordList.contains(ch) ){ wordList.remove((Character)ch); }                
+			}
+		}
+		if(wordList.size() == 0){ return true; }
+		return false;
+	}
 }
