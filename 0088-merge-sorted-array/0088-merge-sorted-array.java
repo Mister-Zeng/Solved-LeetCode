e20fdac0da2f1638536ddda807dec79b2c5bb4ce
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-24 13:47:23 -0500

    Time: 0 ms (100.00%), Space: 42 MB (64.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,15 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] merged = new int[m + n];
-        int nums1Pos = 0;
-        int nums2Pos = 0;
-        int curIndex = 0; 
+        int i = m - 1;
+        int j = n - 1; 
+        int k = m + n - 1; 
         
-        while(m > nums1Pos && n > nums2Pos){
-            if(nums1[nums1Pos] < nums2[nums2Pos]){
-                merged[curIndex] = nums1[nums1Pos];
-                nums1Pos++;
+        while(j >= 0) {
+            if(i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                merged[curIndex] = nums2[nums2Pos];
-                nums2Pos++;
+                nums1[k--] = nums2[j--];
             }
-            curIndex++;
-        }
-        
-        System.arraycopy(nums1, nums1Pos, merged, curIndex, m - nums1Pos);
-        System.arraycopy(nums2, nums2Pos, merged, curIndex, n - nums2Pos);
-        
-        for(int i = 0; i< nums1.length; i++){
-            nums1[i] = merged[i];
         }
     }
 }
