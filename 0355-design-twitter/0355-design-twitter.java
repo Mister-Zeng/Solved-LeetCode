05661e19166acafcac614c12950414c26610c0f7
Jason Zeng <officialjasonzeng@gmail.com>
2024-02-24 12:43:49 -0500

    Time: 8 ms (91.96%), Space: 41.7 MB (30.55%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,66 +1,99 @@
 class Twitter {
+    private static int timeStamp = 0; 
     
-    int time;
-    List<int[]>[] tweets;
-    Set<Integer>[] followees;
-
-    public Twitter() {
+    private Map<Integer, User> userMap; 
+    
+    private class Tweet {
+        public int id; 
+        public int time; 
+        public Tweet next;
         
-        time = 0;
-        tweets = new List[501];
-        followees = new Set[501];
+        public Tweet(int id) {
+            this.id = id; 
+            time = timeStamp++;
+            next = null;
+        }
+    }
+    
+    public class User {
+        public int id;
+        public Set<Integer> followed;
+        public Tweet tweetHead;
+        
+        public User(int id) {
+            this.id = id;
+            followed = new HashSet<>();
+            follow(id);
+            tweetHead = null;
+        }
+        
+        public void follow(int id) {
+            followed.add(id);
+        }
+        
+        public void unfollow(int id) {
+            followed.remove(id);
+        }
+        
+        public void post(int id) {
+            Tweet post = new Tweet(id);
+            post.next = tweetHead;
+            tweetHead = post; 
+        }
+    }
+    
+    public Twitter() {
+        userMap = new HashMap<>();
     }
     
     public void postTweet(int userId, int tweetId) {
-        
-        ++time;
-        if (tweets[userId] == null) {
-            tweets[userId] = new ArrayList<> ();
-            follow (userId, userId);
+        if(!userMap.containsKey(userId)) {
+            User user = new User(userId);
+            userMap.put(userId, user);
         }
-        
-        tweets[userId].add (new int[] {time, tweetId});
+        userMap.get(userId).post(tweetId);
     }
-   
+    
     public List<Integer> getNewsFeed(int userId) {
+        List<Integer> res = new LinkedList<>();
         
-        if (followees[userId] == null) {
-            return new ArrayList<> ();
-        }
+        if(!userMap.containsKey(userId)) return res; 
         
-        List<Integer> newsFeed = new ArrayList<> ();
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<> ((tweet1, tweet2) -> tweet2[0] - tweet1[0]);
-        
-        for (int followee : followees[userId]) {
-            if (tweets[followee] == null) {
-                continue;
-            }
-            
-            for (int[] tweet : tweets[followee]) {
-                maxHeap.offer (tweet);
+        Set<Integer> users = userMap.get(userId).followed; 
+        PriorityQueue<Tweet> q = new PriorityQueue<Tweet>(users.size(), (a, b) -> b.time - a.time);
+        for(int user : users) {
+            Tweet tweet = userMap.get(user).tweetHead;
+            if(tweet != null) {
+                q.add(tweet);
             }
         }
         
-        while (!maxHeap.isEmpty () && newsFeed.size () < 10) {
-            newsFeed.add (maxHeap.poll ()[1]);
+        int n = 0;
+        while(!q.isEmpty() && n < 10) {
+            Tweet tweet = q.poll();
+            res.add(tweet.id);
+            n++;
+            if(tweet.next != null) q.add(tweet.next);
         }
         
-        return newsFeed;
+        return res;
     }
     
     public void follow(int followerId, int followeeId) {
-        
-        if (followees[followerId] == null) {
-            followees[followerId] = new HashSet<> ();
-            followees[followerId].add (followerId);
+        if(!userMap.containsKey(followerId)) {
+            User user = new User(followerId);
+            userMap.put(followerId, user);
         }
-        
-        followees[followerId].add (followeeId);
+        if(!userMap.containsKey(followeeId)) {
+            User user = new User(followeeId);
+            userMap.put(followeeId, user);
+        }
+        userMap.get(followerId).follow(followeeId);
     }
     
     public void unfollow(int followerId, int followeeId) {
-        
-        followees[followerId].remove (followeeId);
+        if(!userMap.containsKey(followerId) || followerId == followeeId) return;
+        userMap.get(followerId).unfollow(followeeId);
     }
 }
 
