930b3a3096c9b66a4d1568544e0f9a08a8e4ecc6
Jason Zeng <officialjasonzeng@gmail.com>
2024-03-06 16:57:58 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (77.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,10 +2,10 @@ class Solution {
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
 
-        int dp0 = 0, dp1 = 0, curr;
+        int dp0 = 0, dp1 = 0;
         
         for(int num : nums) {
-            curr = Math.max(dp0 + num, dp1);
+            int curr = Math.max(dp0 + num, dp1);
             dp0 = dp1;
             dp1 = curr;
         }
