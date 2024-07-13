e6e1580b7566a3d0a41794f049587354ba082fbd
Jason Zeng <officialjasonzeng@gmail.com>
2024-07-13 14:10:56 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (35.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length - 1].length();
+        int length = 0; 
+        boolean letter = false; 
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(letter == false && s.charAt(i) == ' ') {
+                continue;
+            }
+            if(i > 0) letter = true;
+            if(s.charAt(i) == ' ') return length;
+            length++;
+        }
+        
+        return length;
     }
 }
